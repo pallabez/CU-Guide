71079d4b499481b778afcf8db08330320d634e6e
pallabez <pallabsonowal999@gmail.com>
2021-12-23 11:05:33 +0530

    Add sidebar button interaction

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ hamburger.click(hamburgerSwitch);
 
 function hamburgerSwitch() {
     $('.sidebar').toggleClass("sidebar-off");
+    $('.navbar').toggleClass("navbar-minimized");
     //$('main').toggleClass("main-full");
     $('.blanket').toggleClass("blanket-on");
 }
