853bda8ad6ab98f25c5d25474a2a86d5d89e1a1a
kuro <kuro@pop-os.localdomain>
2023-05-16 23:24:31 +0530

    setup vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import vuetify from 'vite-plugin-vuetify'
 
+export default defineNuxtConfig({
+    css: [
+        'vuetify/styles',
+        '@mdi/font/css/materialdesignicons.css'
+    ],
+    build: {
+        transpile: ['vuetify']
+    },
+    modules: [
+        async (options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', config => { config.plugins?.push(vuetify()) })
+        }
+    ],
+    vite: {
+        ssr: {
+            noExternal: ['vuetify']
+        }
+    },
 })
---
+++ plugins/vuetify.js
@@ -0,0 +1,18 @@
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+export default defineNuxtPlugin(nuxtApp => {
+    const vuetify = createVuetify({
+      ssr: true,
+      icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi
+        }
+      },
+    })
+  
+    nuxtApp.vueApp.use(vuetify)
+  })
+  
