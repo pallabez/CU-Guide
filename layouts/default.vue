ab5f9c49f07decf0fce71854338ee12ddc777acc
kuro <kuro@pop-os.localdomain>
2023-05-17 07:24:36 +0530

    completed navigation panel

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,25 +10,7 @@
             <v-icon>mdi-triangle</v-icon>
         </v-system-bar>
 
-        <v-navigation-drawer v-model="drawer">
-            <v-sheet color="grey-lighten-4" class="pa-4">
-                <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
-
-                <div>john@google.com</div>
-            </v-sheet>
-
-            <v-divider></v-divider>
-
-            <v-list>
-                <v-list-item v-for="[icon, text] in links" :key="icon" link>
-                    <template v-slot:prepend>
-                        <v-icon>{{ icon }}</v-icon>
-                    </template>
-
-                    <v-list-item-title>{{ text }}</v-list-item-title>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
+        <app-side-bar />
 
         <v-main>
             <v-container class="py-8 px-6" fluid>
@@ -37,20 +19,6 @@
         </v-main>
     </v-app>
 </template>
-
-<script>
-export default {
-    data: () => ({
-        drawer: null,
-        links: [
-            ['mdi-inbox-arrow-down', 'Inbox'],
-            ['mdi-send', 'Send'],
-            ['mdi-delete', 'Trash'],
-            ['mdi-alert-octagon', 'Spam'],
-        ],
-    }),
-}
-</script>
 
 <style>
 :root {
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,18 +1,40 @@
 import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
+const cuTheme = {
+  dark: false,
+  colors: {
+    background: '#eee',
+    surface: '#FFFFFF',
+    primary: '#ce3d3d',
+    'primary-darken-1': '#922b2b',
+    'primary-light-1': '#d7adad',
+    secondary: '#e20665',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00'
+  }
+}
+
 export default defineNuxtPlugin(nuxtApp => {
-    const vuetify = createVuetify({
-      ssr: true,
-      icons: {
-        defaultSet: 'mdi',
-        aliases,
-        sets: {
-          mdi
-        }
-      },
-    })
-  
-    nuxtApp.vueApp.use(vuetify)
+  const vuetify = createVuetify({
+    ssr: true,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi
+      }
+    },
+    theme: {
+      defaultTheme: 'cuTheme',
+      themes: {
+        cuTheme
+      }
+    }
   })
-  
+
+  nuxtApp.vueApp.use(vuetify)
+})
